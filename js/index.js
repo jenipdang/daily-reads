960d38cc24073ce8976328b6f1c3fd60f520393b
Jeni Dang <jenipdang@yahoo.com>
2021-11-12 07:06:10 -0800

    Update css styling for horoscope display

--- js/index.js
+++ js/index.js
@@ -5,6 +5,13 @@ searchBox().addEventListener('keypress', searchQuery)
 
 const messageBanner = () => document.querySelector("#message-banner")
 
+function searchQuery(event) {
+	if(event.keyCode === 13) {
+		getResults(searchBox().value)
+		// console.log(searchBox().value)
+	}
+}
+
 const getResults = (sign) => {
 	fetch(`https://sameer-kumar-aztro-v1.p.rapidapi.com/?sign=${sign}&day=today`, {
 		"method": "POST",
@@ -21,3 +28,13 @@ const getResults = (sign) => {
 
 	.catch(handleErrorDisplay)
 }
+
+const displayResults = (horoscope) => {
+	document.getElementById('description').innerText = '\"' + horoscope.description + '\"'
+	document.getElementById('current-date').innerText = horoscope.current_date
+	document.getElementById('color').innerText = 'Color: ' + horoscope.color
+	document.getElementById('compatibility').innerText = 'Compatibility: ' + horoscope.compatibility
+	document.getElementById('lucky-number').innerText = 'Lucky Number: ' + horoscope.lucky_number
+}
+
+document.addEventListener("DOMContentLoaded", searchQuery)
