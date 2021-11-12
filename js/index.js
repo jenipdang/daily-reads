0907717062f4c48d2a952ecbf58f5d73f7361e49
Jeni Dang <jenipdang@yahoo.com>
2021-11-12 07:26:02 -0800

    Add another public api to fetch and display random quote when broswer load

--- js/index.js
+++ js/index.js
@@ -69,3 +69,28 @@ const hideMobileMenu = () => {
 
 mobileLinks.addEventListener('click', hideMobileMenu);
 
+async function getQuotes() {
+    await fetch("https://quotes15.p.rapidapi.com/quotes/random/", {
+	    "method": "GET",
+	    "headers": {
+	    	"x-rapidapi-host": "quotes15.p.rapidapi.com",
+		    "x-rapidapi-key": MY_API_KEY
+	    }
+    })
+    .then (response => response.json())
+    .then(response => {
+	    console.log(response);
+
+	    document.getElementById('description').innerHTML = response.content
+	    document.getElementById('color').innerHTML = ' - ' + response.originator.name + ' - '
+    })
+    .catch(handleErrorDisplay)
+}
+
+document.addEventListener("DOMContentLoaded", getQuotes)
+
+const card = document.querySelector('.zodiacs_card_flip')
+
+card.addEventListener('mouseover', function (e) {
+	card.classList.toggle('is-flipped')
+})
